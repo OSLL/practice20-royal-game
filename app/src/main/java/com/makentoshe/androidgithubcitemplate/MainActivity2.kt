3692a1cdfb17edab367e9e5f5ae6c6a162f6e316
Michael <michael.gorodov@gmail.com>
2020-06-16 09:35:26 +0300

    Добавил в xml navbar и переключение режимов через радио кнопки

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity2.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity2.kt
@@ -2,17 +2,10 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
+        setContentView(R.layout.activity_main2)
     }
 }
