698b8a580e83735d906482c5022953bfe037f83d
Michael <michael.gorodov@gmail.com>
2020-06-17 09:47:12 +0300

    Добавил info активити и play/pause анимацию

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.github.mikephil.charting.data.LineData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        infoButton.setOnClickListener{
+            val infoIntent = Intent(this, InfoActivity::class.java)
+            startActivity(infoIntent)
+        }
 
+        playButton.setOnClickListener{
+            pauseButton.animate().alpha(playButton.alpha).setDuration(200)
+            playButton.animate().alpha(1 - playButton.alpha).setDuration(200)
+        }
+
+        /*val lineDataSets = arrayListOf(lineDataSet1, lineDataSet2)
+        channelsChart.data = lineDataSets;
+        channelsChart.invalidate()
+
+        val revenueComp1 = arrayListOf(10000f, 20000f, 30000f, 40000f)
+        val revenueComp2 = arrayListOf(12000f, 23000f, 35000f, 48000f)*/
 
     }
+
 }
