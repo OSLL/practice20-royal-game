a68eb0656ae5be2f4526ed7ef67d9b30f07e2e8c
Alex <sacha1707@gmail.com>
2020-06-18 10:50:33 +0300

    recyclerView First version

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
@@ -14,64 +15,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.data.LineData
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
-
-    class MyAdapter(private val myDataset: Array<String>) :
-        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder.
-        // Each data item is just a string in this case that is shown in a TextView.
-        class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
-
-        // Create new views (invoked by the layout manager)
-        override fun onCreateViewHolder(parent: ViewGroup,
-                                        viewType: Int): MyAdapter.MyViewHolder {
-            // create a new view
-            val textView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.my_text_view, parent, false) as TextView
-            // set the view's size, margins, paddings and layout parameters
-            return MyViewHolder(textView)
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
-            holder.textView.text = myDataset[position]
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        override fun getItemCount() = myDataset.size
-    }
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myDataset = arrayOf("sas", "asa")
 
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = MyAdapter(myDataset)
-
-        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
-
-            // use a linear layout manager
-            layoutManager = viewManager
-
-            // specify an viewAdapter (see also next example)
-            adapter = viewAdapter
-
-        }
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+        recyclerView.layoutManager  = LinearLayoutManager(this)
+        recyclerView.adapter = Adapter(generateFakeValues())
 
         menuButton.setOnClickListener{
             drawer_layout.openDrawer(GravityCompat.START)
@@ -96,5 +50,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun generateFakeValues(): List<String> {
+        val values = mutableListOf<String>()
+        for (i in 0..100){
+            values.add("$i element")
+        }
+        return values
+    }
 
+    class Adapter(private val values: List<String>): RecyclerView.Adapter<Adapter.ViewHolder>(){
+        override fun getItemCount() = values.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.my_text_view, parent, false)
+            return ViewHolder(itemView)
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            var textView: TextView? = null
+            init{
+                textView = itemView?.findViewById(R.id.text_list_item)
+            }
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder?.textView?.text = values[position]
+        }
+    }
 }
